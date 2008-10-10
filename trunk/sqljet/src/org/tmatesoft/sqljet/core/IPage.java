42eaa9d008a42b3f52bf5c3e68522ebc25a4fa36
alex <alex@06c6b4df-3a75-4b2a-9d74-3e57e8079485>
2008-10-10 19:11:13 +0000

    project layout with eclipse project files, build.xml and sample classes.
    git-svn-id: http://svn.svnkit.com/repos/sqljet@2 06c6b4df-3a75-4b2a-9d74-3e57e8079485

---
+++ trunk/sqljet/src/org/tmatesoft/sqljet/core/IPage.java
@@ -0,0 +1,22 @@
+/**
+ * IPage.java
+ * Copyright (C) 2008 TMate Software Ltd
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package org.tmatesoft.sqljet.core;
+
+/**
+ * @author TMate Software Ltd.
+ *
+ */
+public interface IPage {
+
+}

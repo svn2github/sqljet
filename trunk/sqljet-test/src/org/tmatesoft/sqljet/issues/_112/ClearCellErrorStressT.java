803b716b29d95fd6d2a0af31d11d6d3db739abd2
sergey.scherbina <sergey.scherbina@06c6b4df-3a75-4b2a-9d74-3e57e8079485>
2009-11-20 00:44:32 +0000

    ClearCellError test included to stress-test set
    git-svn-id: http://svn.svnkit.com/repos/sqljet@772 06c6b4df-3a75-4b2a-9d74-3e57e8079485

--- trunk/sqljet-test/src/org/tmatesoft/sqljet/issues/_112/ClearCellErrorStressT.java
+++ trunk/sqljet-test/src/org/tmatesoft/sqljet/issues/_112/ClearCellErrorStressT.java
@@ -12,7 +12,7 @@ import org.tmatesoft.sqljet.core.table.ISqlJetTable;
 import org.tmatesoft.sqljet.core.table.ISqlJetTransaction;
 import org.tmatesoft.sqljet.core.table.SqlJetDb;
 
-public class ClearCellError extends AbstractNewDbTest {
+public class ClearCellErrorStressT extends AbstractNewDbTest {
 
     private static final int INSERTS_COUNT = 10000;
 

100d8a52e0f32de1aac1707ed651a85da4c8267f
sergey.scherbina <sergey.scherbina@06c6b4df-3a75-4b2a-9d74-3e57e8079485>
2009-11-21 05:32:00 +0000

    Javadoc update: ISqlJetBusyHadler/SqlJetDefaultBusyHandler
    git-svn-id: http://svn.svnkit.com/repos/sqljet@776 06c6b4df-3a75-4b2a-9d74-3e57e8079485

--- trunk/sqljet/src/org/tmatesoft/sqljet/core/table/ISqlJetBusyHandler.java
+++ trunk/sqljet/src/org/tmatesoft/sqljet/core/table/ISqlJetBusyHandler.java
@@ -17,13 +17,44 @@
  */
 package org.tmatesoft.sqljet.core.table;
 
+import org.tmatesoft.sqljet.core.SqlJetErrorCode;
+import org.tmatesoft.sqljet.core.SqlJetException;
+
 /**
+ * <p>
+ * Busy handler interface.
+ * </p>
+ * 
+ * <p>
+ * Busy handler are used to implement some behavior on database locking if
+ * database is locked already by other thread or process. To method call() is
+ * passed number of retry to obtain database lock. If call() returns true then
+ * retries to locking still continue. If call() returns false then will be
+ * thrown {@link SqlJetException} with {@link SqlJetErrorCode#BUSY}.
+ * </p>
+ * 
  * @author TMate Software Ltd.
  * @author Sergey Scherbina (sergey.scherbina@gmail.com)
- *
+ * 
  */
 public interface ISqlJetBusyHandler {
 
-    boolean call( int number );
-    
+    /**
+     * <p>
+     * Callback which implements busy handler.
+     * </p>
+     * 
+     * <p>
+     * To method call() is passed number of retry to obtain database lock. If
+     * call() returns true then retries to locking still continue. If call()
+     * returns false then will be thrown {@link SqlJetException} with
+     * {@link SqlJetErrorCode#BUSY}.
+     * </p>
+     * 
+     * @param number
+     *            number of retry to obtain lock on database.
+     * @return true if retries will continue or false if retries will stop.
+     */
+    boolean call(int number);
+
 }

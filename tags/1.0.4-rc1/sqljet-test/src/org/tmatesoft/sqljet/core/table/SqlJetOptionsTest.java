3decdf352a59ca0af88ba00ee0a87dba7c6f5cb4
alex <alex@06c6b4df-3a75-4b2a-9d74-3e57e8079485>
2010-12-16 16:17:08 +0000

    SqlJet version 1.0.4-rc1
    git-svn-id: http://svn.svnkit.com/repos/sqljet@962 06c6b4df-3a75-4b2a-9d74-3e57e8079485

---
+++ tags/1.0.4-rc1/sqljet-test/src/org/tmatesoft/sqljet/core/table/SqlJetOptionsTest.java
@@ -0,0 +1,138 @@
+/**
+ * SqlJetOptionsTest.java
+ * Copyright (C) 2009-2010 TMate Software Ltd
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * For information on how to redistribute this software under
+ * the terms of a license other than GNU General Public License
+ * contact TMate Software at support@sqljet.com
+ */
+package org.tmatesoft.sqljet.core.table;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.tmatesoft.sqljet.core.SqlJetEncoding;
+import org.tmatesoft.sqljet.core.SqlJetException;
+import org.tmatesoft.sqljet.core.internal.ISqlJetLimits;
+import org.tmatesoft.sqljet.core.internal.fs.util.SqlJetFileUtil;
+
+/**
+ * @author TMate Software Ltd.
+ * @author Dmitry Stadnik (dtrace@seznam.cz)
+ * 
+ */
+public class SqlJetOptionsTest extends TestCase {
+
+    private File fileDb = new File(SqlJetTableTest.DB);
+    private SqlJetDb db;
+
+    public SqlJetOptionsTest() {
+        super("Options Test");
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        db = SqlJetDb.open(fileDb, true);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        if (db != null) {
+            db.close();
+        }
+    }
+
+    public void testDefaultAutovacuum() throws SqlJetException {
+        ISqlJetOptions options = db.getOptions();
+        assertFalse(options.isAutovacuum());
+        assertFalse(options.isIncrementalVacuum());
+    }
+
+    public void testDefaultCacheSize() throws SqlJetException {
+        ISqlJetOptions options = db.getOptions();
+        assertEquals(2000, options.getCacheSize());
+    }
+
+    public void testDefaultEncoding() throws SqlJetException {
+        ISqlJetOptions options = db.getOptions();
+        assertEquals(SqlJetEncoding.UTF8, options.getEncoding());
+    }
+
+    public void testDefaultSchemaVersion() throws SqlJetException {
+        ISqlJetOptions options = db.getOptions();
+        assertEquals(6, options.getSchemaVersion());
+    }
+
+    public void testDefaultUserVersion() throws SqlJetException {
+        ISqlJetOptions options = db.getOptions();
+        assertEquals(0, options.getUserVersion());
+    }
+
+    public void testDefaultFileFormat() throws SqlJetException {
+        ISqlJetOptions options = db.getOptions();
+        assertEquals(ISqlJetLimits.SQLJET_MAX_FILE_FORMAT, options.getFileFormat());
+    }
+
+    @Test
+    public void legacyFileFormatTrue() throws SqlJetException, FileNotFoundException, IOException {
+
+        final File createFile = File.createTempFile(this.getClass().getSimpleName(), null);
+        createFile.deleteOnExit();
+
+        try {
+
+            final SqlJetDb createDb = SqlJetDb.open(createFile, true);
+            createDb.getOptions().setLegacyFileFormat(true);
+            createDb.close();
+
+            final SqlJetDb openDb = SqlJetDb.open(createFile, true);
+            final int fileFormat = openDb.getOptions().getFileFormat();
+            Assert.assertEquals(ISqlJetLimits.SQLJET_MIN_FILE_FORMAT, fileFormat);
+            
+        } finally {
+
+            SqlJetFileUtil.deleteFile(createFile);
+
+        }
+
+    }
+
+    @Test
+    public void legacyFileFormatFalse() throws SqlJetException, FileNotFoundException, IOException {
+
+        final File createFile = File.createTempFile(this.getClass().getSimpleName(), null);
+        createFile.deleteOnExit();
+
+        try {
+
+            final SqlJetDb createDb = SqlJetDb.open(createFile, true);
+            createDb.getOptions().setLegacyFileFormat(false);
+            createDb.close();
+
+            final SqlJetDb openDb = SqlJetDb.open(createFile, true);
+            final int fileFormat = openDb.getOptions().getFileFormat();
+            Assert.assertFalse(ISqlJetLimits.SQLJET_MIN_FILE_FORMAT == fileFormat);
+            
+        } finally {
+
+            SqlJetFileUtil.deleteFile(createFile);
+
+        }
+
+    }
+    
+}
---
+++ tags/1.0.4-rc1/sqljet-test/src/org/tmatesoft/sqljet/core/table/SqlJetTableStressTest.java
@@ -0,0 +1,83 @@
+/**
+ * SqlJetTableStressTest.java
+ * Copyright (C) 2009-2010 TMate Software Ltd
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * For information on how to redistribute this software under
+ * the terms of a license other than GNU General Public License
+ * contact TMate Software at support@sqljet.com
+ */
+package org.tmatesoft.sqljet.core.table;
+
+import java.io.File;
+
+import junit.framework.TestCase;
+
+import org.tmatesoft.sqljet.core.SqlJetException;
+
+/**
+ * @author TMate Software Ltd.
+ * @author Dmitry Stadnik (dtrace@seznam.cz)
+ * 
+ */
+public class SqlJetTableStressTest extends TestCase {
+
+    private SqlJetDb db;
+
+    public SqlJetTableStressTest() {
+        super("Table Stress Test");
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        File fileDb = File.createTempFile("stressTest", null);
+        fileDb.deleteOnExit();
+        db = SqlJetDb.open(fileDb, true);
+        db.runWriteTransaction(new ISqlJetTransaction() {
+
+            public Object run(SqlJetDb db) throws SqlJetException {
+                db.createTable("create table t (c1 text, c2 int)");
+                return null;
+            }
+        });
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        db.close();
+    }
+
+    public void testInsert100000Records() throws Exception {
+        final ISqlJetTable t = db.getTable("t");
+        db.runWriteTransaction(new ISqlJetTransaction() {
+
+            public Object run(SqlJetDb db) throws SqlJetException {
+                for (int i = 0; i < 100000; i++) {
+                    t.insert("Bolshie nogi shli po doroge!", i);
+                }
+                return null;
+            }
+        });
+
+        db.runReadTransaction(new ISqlJetTransaction() {
+
+            public Object run(SqlJetDb db) throws SqlJetException {
+                ISqlJetCursor c = t.open();
+                c.last();
+                c.previous();
+                assertTrue(c.getString(0).indexOf("nogi") > 0);
+                assertEquals(99998, c.getInteger(1));
+                return null;
+            }
+        });
+
+    }
+}

90be03ce66d50ee3fb2378c2396d2e74b520e113
alex <alex@06c6b4df-3a75-4b2a-9d74-3e57e8079485>
2011-08-03 14:00:42 +0000

    Update used to fail when updating records with blobs without specifying new blob value.
    git-svn-id: http://svn.svnkit.com/repos/sqljet@1154 06c6b4df-3a75-4b2a-9d74-3e57e8079485

--- trunk/sqljet/src/test/java/org/tmatesoft/sqljet/core/table/BlobsTest.java
+++ trunk/sqljet/src/test/java/org/tmatesoft/sqljet/core/table/BlobsTest.java
@@ -1,6 +1,8 @@
 package org.tmatesoft.sqljet.core.table;
 
 import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -61,6 +63,83 @@ public class BlobsTest extends AbstractNewDbTest {
             }
         });
 
+    }
+    
+    @Test
+    public void updateButBlob() throws Exception {
+
+        db.getOptions().setAutovacuum(true);
+        db.runWriteTransaction(new ISqlJetTransaction() {
+            public Object run(SqlJetDb db) throws SqlJetException {
+                db.createTable(T_DDL);
+                return true;
+            }
+        });
+
+        final SecureRandom rnd = new SecureRandom();
+        final ISqlJetTable t = db.getTable("t");
+        final byte[] blob = "text".getBytes();
+
+        db.runWriteTransaction(new ISqlJetTransaction() {
+            public Object run(SqlJetDb db) throws SqlJetException {
+                try {
+                    for(int i = 0; i < 10000; i++) {
+                        t.insert(rnd.nextInt(2048), blob);
+                    }
+                } catch (SqlJetException e) {
+                    if (!SqlJetErrorCode.CONSTRAINT.equals(e.getErrorCode())) {
+                        throw e;
+                    }
+                }
+                return true;
+            }
+        });
+
+        db.runWriteTransaction(new ISqlJetTransaction() {
+            public Object run(SqlJetDb db) throws SqlJetException {
+                final ISqlJetCursor c = t.open();
+                Map<String, Object> values = new HashMap<String, Object>();
+                try {
+                    if (!c.eof()) {
+                        do {                            
+                            while (true) {
+                                values.put("x", rnd.nextInt(2048) + 1024);
+                                try {
+                                    c.updateByFieldNames(values);
+                                    break;
+                                } catch (SqlJetException e) {
+                                    if (!SqlJetErrorCode.CONSTRAINT.equals(e.getErrorCode())) {
+                                        throw e;
+                                    }
+                                }
+                            }
+                        } while (c.next());
+                    }
+                } finally {
+                    c.close();
+                }
+                return null;
+            }
+        });
+
+        db.runReadTransaction(new ISqlJetTransaction() {
+            public Object run(SqlJetDb db) throws SqlJetException {
+                final ISqlJetCursor c = t.open();
+                try {
+                    if (!c.eof()) {
+                        do {
+                            byte[] b = c.getBlobAsArray("image");
+                            long xValue = c.getInteger("x");
+                            Assert.assertArrayEquals(b, blob);
+                            Assert.assertTrue(xValue >= 1024);
+                        } while (c.next());
+                    }
+                } finally {
+                    c.close();
+                }
+                return null;
+            }
+        });
     }
 
     @Test

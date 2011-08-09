cf3f040cf9c33b5f6c67f9c7690d9dd55109073f
sergey.scherbina <sergey.scherbina@06c6b4df-3a75-4b2a-9d74-3e57e8079485>
2011-08-09 18:55:21 +0000

    Test for "Issue 160:Cannot read table definition if two consecutive FOREIGN KEY definitions are found without being separated by comma" http://code.google.com/p/sqljet/issues/detail?id=160
    git-svn-id: http://svn.svnkit.com/repos/sqljet@1156 06c6b4df-3a75-4b2a-9d74-3e57e8079485

---
+++ trunk/sqljet/src/test/java/org/tmatesoft/sqljet/issues/_160/TwoForeignTest.java
@@ -0,0 +1,18 @@
+package org.tmatesoft.sqljet.issues._160;
+
+import org.junit.Test;
+import org.tmatesoft.sqljet.core.AbstractNewDbTest;
+import org.tmatesoft.sqljet.core.SqlJetException;
+
+public class TwoForeignTest extends AbstractNewDbTest {
+
+	@Test
+	public void test() throws SqlJetException {
+		db.createTable("CREATE TABLE dummy_table ("
+				+ "id integer PRIMARY KEY AUTOINCREMENT NOT NULL,"
+				+ "content_id INTEGER," + "content_page_id INTEGER,"
+				+ "FOREIGN KEY(content_id) REFERENCES content(id)"
+				+ "FOREIGN KEY(content_page_id) REFERENCES content_page(id)"
+				+ ")");
+	}
+}

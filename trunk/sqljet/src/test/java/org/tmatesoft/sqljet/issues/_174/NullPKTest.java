60b992e97809ad25f5c2ffed5cb207ca5e7cc235
sergey.scherbina <sergey.scherbina@06c6b4df-3a75-4b2a-9d74-3e57e8079485>
2013-04-16 08:58:29 +0000

    Test for issue:  Issue 174:	ISqlJetTable#insert works incorrectly if the first value is 'null' and the table's first field is a primary key  http://code.google.com/p/sqljet/issues/detail?id=174
    git-svn-id: http://svn.svnkit.com/repos/sqljet@1282 06c6b4df-3a75-4b2a-9d74-3e57e8079485

---
+++ trunk/sqljet/src/test/java/org/tmatesoft/sqljet/issues/_174/NullPKTest.java
@@ -0,0 +1,53 @@
+package org.tmatesoft.sqljet.issues._174;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.tmatesoft.sqljet.core.AbstractNewDbTest;
+import org.tmatesoft.sqljet.core.SqlJetException;
+import org.tmatesoft.sqljet.core.SqlJetTransactionMode;
+import org.tmatesoft.sqljet.core.table.ISqlJetCursor;
+import org.tmatesoft.sqljet.core.table.ISqlJetTable;
+import org.tmatesoft.sqljet.core.table.SqlJetDb;
+
+public class NullPKTest extends AbstractNewDbTest {
+
+	@Test
+	public void testNulPK() throws SqlJetException {
+		final SqlJetDb sqlJetDb = db;
+		sqlJetDb.createTable("CREATE TABLE MY_TABLE (id INTEGER NOT NULL PRIMARY KEY, field1 TEXT, field2 TEXT)");
+
+		sqlJetDb.beginTransaction(SqlJetTransactionMode.WRITE);
+
+		ISqlJetTable table = sqlJetDb.getTable("MY_TABLE");
+		table.insert("value1", "value2");
+		table.insert(null, "value2");
+		sqlJetDb.commit();
+
+		sqlJetDb.beginTransaction(SqlJetTransactionMode.READ_ONLY);
+		table = sqlJetDb.getTable("MY_TABLE");
+		final ISqlJetCursor cursor = table.open();
+		int i = 0;
+		while (!cursor.eof()) {
+			final String value1 = cursor.getString(1);
+			final String value2 = cursor.getString(2);
+
+			switch (i) {
+			case 0:
+				Assert.assertEquals("value1", value1);
+				Assert.assertEquals("value2", value2);
+				break;
+			case 1:
+				Assert.assertEquals(null, value1);
+				Assert.assertEquals("value2", value2);
+				break;
+			default:
+				break;
+			}
+
+			cursor.next();
+			i++;
+		}
+		sqlJetDb.commit();
+	}
+
+}
